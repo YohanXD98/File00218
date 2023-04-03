3af0af992e02574cc7987d812a32dba935132db0
YohanXD <127111376+YohanXD98@users.noreply.github.com>
2023-04-03 15:08:31 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+<script>
+    function validarLogin() {
+        var username = document,GetElementById("username").value;
+        var passowrd = document,getElementById("password").value;
+        if9username == "Marcos" && password =="senha1230") {
+            alert("Login realizado com sucesso")
+        } else {
+            alert("Nome de usuário ou senha incorretos. Tente novamente.");
+        }
+    }    
+</script>
